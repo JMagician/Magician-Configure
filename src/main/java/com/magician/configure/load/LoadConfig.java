7d75439f986be575945d4b52e7e2d492647eed54
Beerus <yuyemail@163.com>
2022-07-12 19:39:52 +0800

    首次提交

--- src/main/java/com/magician/configure/load/LoadConfig.java
+++ src/main/java/com/magician/configure/load/LoadConfig.java
@@ -5,26 +5,36 @@ import com.magician.configure.load.constant.ReadMode;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Properties;
 
 public class LoadConfig {
 
+    private final String CHARSET_NAME = "UTF-8";
+
     public void load(String path, ReadMode readMode) throws Exception {
-        InputStream inputStream = getFileContent(path, readMode);
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
         try {
+            inputStream = getFileContent(path, readMode);
+            inputStreamReader = new InputStreamReader(inputStream, CHARSET_NAME);
+
             Properties p = new Properties();
-            p.load(inputStream);
+            p.load(inputStreamReader);
 
             for(Object key : p.keySet()){
                 String keyStr = key.toString();
-                String value = p.getProperty(keyStr, System.getenv(keyStr));
-                ConfigCacheManager.add(keyStr, new String(value.getBytes("ISO8859-1"), "utf-8"));
+                String value = p.getProperty(keyStr);
+                ConfigCacheManager.add(keyStr, value);
             }
         } catch (Exception e) {
             throw e;
         } finally {
+            if(inputStreamReader != null){
+                inputStreamReader.close();
+            }
             if(inputStream != null){
                 inputStream.close();
             }
