d1b83959b829330faf9393f4dc98461dad40214a
Beerus <yuyemail@163.com>
2022-07-12 19:42:21 +0800

    首次提交

--- src/main/java/com/magician/configure/load/LoadConfig.java
+++ src/main/java/com/magician/configure/load/LoadConfig.java
@@ -7,7 +7,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Properties;
 
 public class LoadConfig {
